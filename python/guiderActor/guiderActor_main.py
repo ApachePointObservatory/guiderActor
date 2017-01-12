abf8cd90d13f812223900bc848944e656a352b33
José Sánchez-Gallego <gallegoj@uw.edu>
2017-01-12 12:09:11 -0800

    added method to set up location when running the actor

--- python/guiderActor/guiderActor_main.py
+++ python/guiderActor/guiderActor_main.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 """Start the guiderActor."""
 
+import sys
 from guiderActor import Msg, GuiderActor
 
 # start a new actor
 if __name__ == "__main__":
-    guider = GuiderActor.GuiderActor.newActor()
+    location = None if len(sys.argv) == 1 else sys.argv[1]
+    guider = GuiderActor.GuiderActor.newActor(location=location)
     guider.run(Msg=Msg)
